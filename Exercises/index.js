b112e21c32c1e852e0e881481bae187201790b62
lx kish <urban.ktulhu@gmail.com>
2019-07-27 15:32:58 +1200

    first commit

---
+++ Exercises/index.js
@@ -0,0 +1,12 @@
+let customer = prompt('Your name is');
+let company = prompt('Your company is');
+
+alert('Hello ' + (customer && company && 'Valued Customer'));
+
+// alert('I\'m working');
+
+// alert = function() {
+//     return 'Another try';
+// }
+// console.log(alert());
+//console.log('I\'m working!');
