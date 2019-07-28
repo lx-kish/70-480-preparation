d4d23f89d0327cbc6ed2e6a7561864d9bb67da7e
lx kish <urban.ktulhu@gmail.com>
2019-07-29 08:29:18 +1200

    fixed error - added drawCanvas() function to graphics.js

--- cbtnuggets.com/03-HTML5-interaction/javascript/graphics.js
+++ cbtnuggets.com/03-HTML5-interaction/javascript/graphics.js
@@ -9,3 +9,46 @@ function changeColor() {
     curColor = (curColor + 1) % colors.length;
     
 }
+
+function drawCanvas() {
+
+    let c = document.getElementById("gg-canvas");
+    var ctx = c.getContext("2d");
+
+    //Add shadow offset
+    ctx.shadowColor = "#000";
+    ctx.shadowBlur = 4;
+    ctx.shadowOffsetX = 10;
+    ctx.shadowOffsetY = 10;
+
+    //Draw yellow circles
+    ctx.beginPath();
+    ctx.fillStyle = "#FFD200";
+    ctx.arc(75, 75, 50, 0, 2 * Math.PI, true);
+    ctx.fill();
+
+    ctx.beginPath();
+    ctx.fillStyle = "#FFD200";
+    ctx.arc(100, 165, 40, 0, 2 * Math.PI, true);
+    ctx.fill();
+
+    ctx.beginPath();
+    ctx.fillStyle = "#FFD200";
+    ctx.arc(125, 23, 20, 0, 2 * Math.PI, true);
+    ctx.fill();
+
+    ctx.beginPath();
+    ctx.fillStyle = "#FFD200";
+    ctx.arc(180, 100, 40, 0, 2 * Math.PI, true);
+    ctx.fill();
+
+    ctx.beginPath();
+    ctx.fillStyle = "#FFD200";
+    ctx.arc(210, 190, 50, 0, 2 * Math.PI, true);
+    ctx.fill();
+
+    ctx.beginPath();
+    ctx.fillStyle = "#FFD200";
+    ctx.arc(225, 58, 20, 0, 2 * Math.PI, true);
+    ctx.fill();
+}
